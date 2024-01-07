60bbf28331568dcb7515a86a66164dc2b0483f4b
Joannabubu <joannabuhuang@gmail.com>
2024-01-07 23:02:03 +0800

    更改穀物線條顏色

--- finance_final.py
+++ finance_final.py
@@ -9,16 +9,9 @@ fred = Fred(api_key='4e4a4ea389197ac12ee3036ee4cff0fc')
 data_wheat = fred.get_series('PWHEAMTUSDM','1/1/2010')
 data_corn = fred.get_series('PMAIZMTUSDM','1/1/2010')
 data_soybeans = fred.get_series('PSOYBUSDM','1/1/2010')
+data_CPI = fred.get_series('USACPALTT01CTGYM', '1/1/2010')
 
-
-
-data_wheat
-data_corn
-data_soybeans
-
-
-
-
+print(data_CPI)
 data_wheat.plot()
 data_corn.plot()
 data_soybeans.plot()
@@ -60,83 +53,8 @@ ax_soybeans.plot(df_merged.index,df_merged['PSOYBUSDM'],color = 'slategray')
 
 
 
-import matplotlib.pyplot as plt
-
-# 創建一個包含三個子圖的畫布
-fig, (ax_wheat, ax_corn, ax_soybeans) = plt.subplots(3, 1, sharex=True, figsize=(10, 8))
-
-# 繪製每個子圖的資料
-ax_wheat.plot(df_merged.index, df_merged['PWHEAMTUSDM'], color='tan', label='Wheat')
-ax_corn.plot(df_merged.index, df_merged['PMAIZMTUSDM'], color='lightseagreen', label='Corn')
-ax_soybeans.plot(df_merged.index, df_merged['PSOYBUSDM'], color='slategray', label='Soybeans')
-
-# 設置標籤和標題
-ax_wheat.set_ylabel('Price (USD)')
-ax_corn.set_ylabel('Price (USD)')
-ax_soybeans.set_ylabel('Price (USD)')
-ax_soybeans.set_xlabel('Date')
-
-# 添加圖例
-ax_wheat.legend()
-ax_corn.legend()
-ax_soybeans.legend()
-
-# 顯示圖形
-plt.show()
 
 
-import matplotlib.pyplot as plt
-
-# 創建一張包含三條相關趨勢圖的線的圖表
-fig, (ax_wheat, ax_corn, ax_soybeans) = plt.subplots(3, 1, figsize=(10, 8), sharex=True)
-
-# 繪製每條趨勢圖的線
-ax_wheat.plot(df_merged.index, df_merged['PWHEAMTUSDM'], color='tan', label='Wheat')
-ax_corn.plot(df_merged.index, df_merged['PMAIZMTUSDM'], color='lightseagreen', label='Corn')
-ax_soybeans.plot(df_merged.index, df_merged['PSOYBUSDM'], color='slategray', label='Soybeans')
-
-# 設置標籤、標題等
-ax_wheat.set_ylabel('Wheat Price (USD)')
-ax_corn.set_ylabel('Corn Price (USD)')
-ax_soybeans.set_ylabel('Soybeans Price (USD)')
-ax_soybeans.set_xlabel('Date')
-
-# 增加圖例
-ax_wheat.legend()
-ax_corn.legend()
-ax_soybeans.legend()
-
-# 顯示圖表
-plt.show()
-
-
-
-import matplotlib.pyplot as plt
-
-# 創建一個包含三個子圖的圖表
-fig, (ax_wheat, ax_corn, ax_soybeans) = plt.subplots(3, 1, sharex=True, figsize=(10, 8))
-
-# 繪製三條趨勢線
-ax_wheat.plot(df_merged.index, df_merged['PWHEAMTUSDM'], color='tan', label='Wheat')
-ax_corn.plot(df_merged.index, df_merged['PMAIZMTUSDM'], color='lightseagreen', label='Corn')
-ax_soybeans.plot(df_merged.index, df_merged['PSOYBUSDM'], color='slategray', label='Soybeans')
-
-# 設定標題和標籤
-ax_wheat.set(title='Wheat Price Trend')
-ax_corn.set(title='Corn Price Trend')
-ax_soybeans.set(title='Soybeans Price Trend')
-plt.xlabel('Date')
-
-# 顯示圖例
-ax_wheat.legend()
-ax_corn.legend()
-ax_soybeans.legend()
-
-# 調整子圖的間距
-plt.tight_layout()
-
-# 顯示圖表
-plt.show()
 
 
 
@@ -161,6 +79,8 @@ ax.set_ylabel('Price (in USD)')
 # 顯示圖形
 plt.show()
 fig.savefig('穀物折線圖.jpg',format = 'jpeg')
+
+
 
 
 
