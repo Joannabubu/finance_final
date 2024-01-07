2f283563f6acee81d7c8f5f01f3d8bdac2086419
Joannabubu <joannabuhuang@gmail.com>
2024-01-07 23:11:45 +0800

    no message

--- finance_final.py
+++ finance_final.py
@@ -11,17 +11,19 @@ data_corn = fred.get_series('PMAIZMTUSDM','1/1/2010')
 data_soybeans = fred.get_series('PSOYBUSDM','1/1/2010')
 data_CPI = fred.get_series('USACPALTT01CTGYM', '1/1/2010')
 
-print(data_CPI)
+#print(data_CPI)
 data_wheat.plot()
 data_corn.plot()
 data_soybeans.plot()
+data_CPI.plot()
 
 
 #讀取資料
 frame_wheat = data_wheat.to_frame(name = 'PWHEAMTUSDM')
 frame_corn = data_corn.to_frame(name = 'PMAIZMTUSDM')
 frame_soybeans = data_soybeans.to_frame(name = 'PSOYBUSDM')
-frame_wheat,frame_corn,frame_soybeans
+frame_CPI = data_CPI.to_frame(name='USACPALTT01CTGYM')
+frame_wheat,frame_corn,frame_soybeans,frame_CPI
 
 
 
@@ -36,37 +38,18 @@ import matplotlib.pyplot as plt
 
 
 
-
-#繪製基本圖形
-plt.plot(df_merged.index, df_merged['PWHEAMTUSDM'])
-plt.plot(df_merged.index, df_merged['PMAIZMTUSDM'])
-plt.plot(df_merged.index, df_merged['PSOYBUSDM'])
-
-
-
-fig,ax_wheat = plt.subplots()
-fig,ax_corn = plt.subplots()
-fig,ax_soybeans = plt.subplots()
-ax_wheat.plot(df_merged.index,df_merged['PWHEAMTUSDM'],color = 'tan')
-ax_corn.plot(df_merged.index,df_merged['PMAIZMTUSDM'],color = 'lightseagreen')
-ax_soybeans.plot(df_merged.index,df_merged['PSOYBUSDM'],color = 'slategray')
-
-
-
-
-
-
-
-
-import matplotlib.pyplot as plt
-
+#繪製穀物趨勢圖
 # 創建一個圖和一個 Axes 對象
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(10,8))
+
+#set the background color
+fig.patch.set_facecolor('white')
+ax.set_facecolor('white')
 
 # 繪製三條線
-ax.plot(df_merged.index, df_merged['PWHEAMTUSDM'], color='tan', label='Wheat')
-ax.plot(df_merged.index, df_merged['PMAIZMTUSDM'], color='lightseagreen', label='Corn')
-ax.plot(df_merged.index, df_merged['PSOYBUSDM'], color='slategray', label='Soybeans')
+ax.plot(df_merged.index, df_merged['PWHEAMTUSDM'], color='slategray', label='Wheat')
+ax.plot(df_merged.index, df_merged['PMAIZMTUSDM'], color='steelblue', label='Corn')
+ax.plot(df_merged.index, df_merged['PSOYBUSDM'], color='cadetblue', label='Soybeans')
 
 # 添加圖例
 ax.legend()
@@ -78,11 +61,23 @@ ax.set_ylabel('Price (in USD)')
 
 # 顯示圖形
 plt.show()
-fig.savefig('穀物折線圖.jpg',format = 'jpeg')
+fig.savefig('穀物折線圖.jpg',format = 'jpeg',dpi=300, facecolor='white')
 
 
+#繪製 CPI 圖型!
+df_merged2 = pd.concat([frame_CPI],axis=1)
+fig, ax = plt.subplots()
 
+# 繪製三條線
+ax.plot(df_merged2.index, df_merged2['USACPALTT01CTGYM'], color='peru', label='CPI')
 
+ax.legend()
+
+ax.set_title('CPI : All Items : Total for USA')
+ax.set_xlabel('year')
+ax.set_ylabel('Contribution to annual inflation')
+
+plt.show()
 
 
 
